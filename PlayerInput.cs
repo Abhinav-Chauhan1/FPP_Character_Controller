9cfee516057a04d44c85330069f43a512d5f532c
Abhinav Chauhan <70648434+Abhinav-Chauhan1@users.noreply.github.com>
2021-01-04 17:28:45 +0530

    Add files via upload

---
+++ PlayerInput.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    void Start()
+    {
+        //Lock Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public bool CanProcessInput()
+    {
+        return Cursor.lockState == CursorLockMode.Locked;
+    }
+
+    //Sprint
+    public bool GetSprintInputHeld()
+    {
+        if (CanProcessInput())
+        {
+            return Input.GetKey(KeyCode.LeftShift);
+        }
+
+        return false;
+    }
+    //Crouch
+    public bool GetCrouchInputDown()
+    {
+        if (CanProcessInput())
+        {
+            return Input.GetKeyDown(KeyCode.C);
+        }
+
+        return false;
+    }
+    //Crouch Over
+    public bool GetCrouchInputReleased()
+    {
+        if (CanProcessInput())
+        {
+            return Input.GetKeyUp(KeyCode.C);
+        }
+
+        return false;
+    }
+
+    //Jump
+    public bool GetJumpInputDown()
+    {
+        if (CanProcessInput())
+        {
+            return Input.GetKeyDown(KeyCode.Space);
+        }
+
+        return false;
+    }
+}
